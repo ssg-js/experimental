1f4a1cb2414584817519dc8cec36037f441eb59e
ssg-js <76690497+ssg-js@users.noreply.github.com>
2024-01-25 10:39:22 +0900

    feat: store user info

--- web/movie_service/app.js
+++ web/movie_service/app.js
@@ -1,7 +1,32 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-loginButton.addEventListener('click', () => {
-  console.log(loginInput.value);
-})
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
+function onLoginSubmit(e) {
+  e.preventDefault();
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreetings(username);
+  console.log(username);
+
+}
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+  // show greeting
+  paintGreetings(savedUsername);
+}
