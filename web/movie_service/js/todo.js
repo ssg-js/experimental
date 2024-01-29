60bd759e79e365cc589536abcbeb3d61292b9087
ssg-js <76690497+ssg-js@users.noreply.github.com>
2024-01-29 14:58:55 +0900

    feat: todolist create/delete

---
+++ web/movie_service/js/todo.js
@@ -0,0 +1,32 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+
+function deleteTodo(e) {
+  const li = e.target.parentElement;
+  li.remove();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+};
+
+function handleToDoSubmit(e) {
+  e.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  paintToDo(newTodo);
+};
+
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
