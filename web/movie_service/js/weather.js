da99a2397b98f935bca661400fe84a8e09d4724d
ssg-js <76690497+ssg-js@users.noreply.github.com>
2024-01-30 11:31:50 +0900

    feat: 기능 완성

--- web/movie_service/js/weather.js
+++ web/movie_service/js/weather.js
@@ -1,8 +1,17 @@
+const WEATHER_KEY = "weather";
+
 function onGeoSuccess(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${config.WEATHER_KEY}&units=matrix`;
-  fetch(weather_url).then(response => response.json()).then(data => console.log(data.name, data.weather[0].main));
+  const weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${config.WEATHER_API_KEY}&units=metric`;
+  fetch(weather_url)
+    .then(response => response.json())
+    .then(data => {
+      const weather = document.querySelector(`#${WEATHER_KEY} span:first-child`);
+      const city = document.querySelector(`#${WEATHER_KEY} span:last-child`);
+      weather.innerText = `${data.weather[0].main} ${data.main.temp}`;
+      city.innerText = data.name;
+    });
 }
 
 function onGeoError() {
