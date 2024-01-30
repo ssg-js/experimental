a84aa9a3ad8cf9618aae68ab578bea8e17e83057
ssg-js <76690497+ssg-js@users.noreply.github.com>
2024-01-30 11:17:47 +0900

    feat: get weather info

--- web/movie_service/js/weather.js
+++ web/movie_service/js/weather.js
@@ -1,10 +1,8 @@
-
-const API_KEY = config.WEATHER_KEY;
-
 function onGeoSuccess(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log(`you live in ${lat} ${lng}`);
+  const lon = position.coords.longitude;
+  const weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${config.WEATHER_KEY}&units=matrix`;
+  fetch(weather_url).then(response => response.json()).then(data => console.log(data.name, data.weather[0].main));
 }
 
 function onGeoError() {
